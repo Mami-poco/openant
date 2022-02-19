320710f2cdcb2d36ce0eec8e099659ed3046231d
Mami <65227712+Mami-poco@users.noreply.github.com>
2022-02-19 16:54:49 +0000

    Update driver.py

--- ant/base/driver.py
+++ ant/base/driver.py
@@ -213,13 +213,7 @@ try:
 
             # get an endpoint instance
             cfg = dev.get_active_configuration()
-            interface_number = cfg[(0, 0)].bInterfaceNumber
-            alternate_setting = usb.control.get_interface(dev, interface_number)
-            intf = usb.util.find_descriptor(
-                cfg,
-                bInterfaceNumber=interface_number,
-                bAlternateSetting=alternate_setting,
-            )
+            intf = cfg[(0, 0)]
 
             self._out = usb.util.find_descriptor(
                 intf,
@@ -249,7 +243,7 @@ try:
             pass
 
         def read(self):
-            return self._in.read(self._in. wMaxPacketSize)
+            return self._in.read(4096)
 
         def write(self, data):
             self._out.write(data)
